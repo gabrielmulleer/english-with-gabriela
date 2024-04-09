0ddc8c683244ff99d7660b33fdd6650c8639a5a0
gabrielmulleer <emaildegabrielm@gmail.com>
2024-04-09 01:23:12 -0300

    feat: add footer section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./app/About";
 import Benefits from "./app/Benefits";
 import Faq from "./app/Faq";
+import Footer from "./app/Footer";
 import Home from "./app/Home";
 import HowItWorks from "./app/HowItWorks";
 import Plans from "./app/Plans";
@@ -15,6 +16,7 @@ function App() {
       <Plans />
       <About />
       <Faq />
+      <Footer />
     </>
   );
 }
