c44b7fc5727efba008b19f126105f0264550b258
gabrielmulleer <emaildegabrielm@gmail.com>
2024-04-09 19:27:53 -0300

    feat: add tablet support to the benefits component

--- src/app/Benefits/index.tsx
+++ src/app/Benefits/index.tsx
@@ -4,45 +4,55 @@ import GlobeIcon from "../../lib/assets/icons/globe.svg";
 import RocketIcon from "../../lib/assets/icons/rocket.svg";
 import HeartIcon from "../../lib/assets/icons/heart.svg";
 export default function Benefits() {
+  type Item = {
+    id: number;
+    icon: string;
+    title: string;
+    content: React.ReactNode;
+  };
+
+  const items: Item[] = [
+    {
+      id: 1,
+      icon: TargetIcon,
+      title: "Cultural Insights",
+      content:
+        "Delve into the diversity of English-speaking cultures to broaden your perspective.",
+    },
+    {
+      id: 2,
+      icon: GlobeIcon,
+      title: " Global Community",
+      content:
+        "Connect with students from around the world and expand your cultural horizons.",
+    },
+    {
+      id: 3,
+      icon: RocketIcon,
+      title: "Real Results",
+      content:
+        "Boost your speaking skills, grow your confidence, and achieve smoother fluency in English.",
+    },
+    {
+      id: 4,
+      icon: HeartIcon,
+      title: "Comprehensive Support",
+      content:
+        "Benefit from ongoing support and guidance from experienced instructor dedicated to your success.",
+    },
+  ];
   return (
     <section id="benefits" className="px-6 py-4">
       <h2 className="text-xl/6 mb-6 mt-4 font-bold text-[#333333]">BENEFITS</h2>
-      <BenefitsCard>
-        <img src={TargetIcon} alt="" />
-        <h3 className="text-lg/6 text-[#F7567C] font-bold">
-          Cultural Insights
-        </h3>
-        <p className="text-start text-[#333333]">
-          Delve into the diversity of English-speaking cultures to broaden your
-          perspective.
-        </p>
-      </BenefitsCard>
-      <BenefitsCard>
-        <img src={GlobeIcon} alt="" />
-        <h3 className="text-lg/6 text-[#F7567C] font-bold">Global Community</h3>
-        <p className="text-start text-[#333333]">
-          Connect with students from around the world and expand your cultural
-          horizons.
-        </p>
-      </BenefitsCard>
-      <BenefitsCard>
-        <img src={RocketIcon} alt="" />
-        <h3 className="text-lg/6 text-[#F7567C] font-bold">Real Results</h3>
-        <p className="text-start text-[#333333]">
-          Boost your speaking skills, grow your confidence, and achieve smoother
-          fluency in English.
-        </p>
-      </BenefitsCard>
-      <BenefitsCard>
-        <img src={HeartIcon} alt="" />
-        <h3 className="text-lg/6 text-[#F7567C] font-bold">
-          Comprehensive Support
-        </h3>
-        <p className="text-start text-[#333333]">
-          Benefit from ongoing support and guidance from experienced instructor
-          dedicated to your success.
-        </p>
-      </BenefitsCard>
+      <div className="flex flex-col md:flex-row md:flex-wrap md:gap-x-6 md:justify-center">
+        {items.map((item) => (
+          <BenefitsCard>
+            <img src={item.icon} alt="" />
+            <h3 className="text-lg/6 text-[#F7567C] font-bold">{item.title}</h3>
+            <p className="text-start text-[#333333]">{item.content}</p>
+          </BenefitsCard>
+        ))}
+      </div>
 
       <button className="text-[#FCFCFC] bg-[#F7567C] text-xs w-full mb-4">
         Sign up today to elevate your English!
--- src/lib/assets/components/AppComponents/Benefits/BenefitsCard/index.tsx
+++ src/lib/assets/components/AppComponents/Benefits/BenefitsCard/index.tsx
@@ -3,7 +3,7 @@ interface BenefitsProps {
 }
 export default function BenefitsCard(props: BenefitsProps) {
   return (
-    <div className="p-4 mb-6 flex items-center flex-col gap-2 shadow-[0px_4px_8px_0px_rgba(0,0,0,0.25)] rounded-lg">
+    <div className="p-4 mb-6 flex items-center flex-col gap-2 shadow-[0px_4px_8px_0px_rgba(0,0,0,0.25)] rounded-lg md:w-[48%]">
       {props.children}
     </div>
   );
