776c2ac03eb6d8720925e17cb8dc7cedadb74f31
gabrielmulleer <emaildegabrielm@gmail.com>
2024-04-11 00:02:39 -0300

    feat: add big screens support to the footer componenet

--- src/app/Footer/index.tsx
+++ src/app/Footer/index.tsx
@@ -5,15 +5,17 @@ export default function Footer() {
   return (
     <section
       id="footer"
-      className="bg-[#F7567C5E] pt-4 pb-8 flex flex-col items-center gap-6"
+      className="bg-[#F7567C5E] py-8 flex flex-col items-center gap-6 lg:flex-row lg:justify-between lg:px-48"
     >
-      <div className="flex items-center gap-4">
-        <img src={instagramIcon} alt="" />
-        <img src={youtubeIcon} alt="" />
-        <img src={tiktokIcon} alt="" />
-      </div>
-      <div className="text-sm/4 font-bold text-[#333333]">
-        support@englishwithgabriela.com
+      <div className="flex flex-col items-center gap-6 lg:flex-row-reverse">
+        <div className="flex items-center gap-4">
+          <img src={instagramIcon} alt="" />
+          <img src={youtubeIcon} alt="" />
+          <img src={tiktokIcon} alt="" />
+        </div>
+        <div className="text-sm/4 font-bold text-[#333333]">
+          support@englishwithgabriela.com
+        </div>
       </div>
       <div className="text-sm/4 font-bold text-[#333333]">
         © All rights reserved{" "}
